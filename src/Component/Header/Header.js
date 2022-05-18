6fa996da8d16404e9aaee04c9f0a57bf4b949eee
alahad102 <alahad102@gmail.com>
2022-05-18 08:49:48 +0600

    navigation done

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,45 @@
+import { signOut } from 'firebase/auth';
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Link } from 'react-router-dom';
+
+import navbar from '../../images/logo/navbar.jpg'
+import auth from '../../firebase.init';
+
+const Header = () => {
+    const [user] = useAuthState(auth);
+
+    const handleSignOut = () => {
+        signOut(auth);
+    }
+    return (
+        <Navbar collapseOnSelect expand="lg" sticky='top' bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand as={Link} to="/">
+                    <img height={40} src={navbar} alt="" /></Navbar.Brand>
+                <Navbar.Brand as={Link} to="/home">My PhotoGraphy</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to="services">Services</Nav.Link>
+                        <Nav.Link as={Link} to="reviews">Reviews</Nav.Link>
+                        <Nav.Link as={Link} to="blogs">Blogs</Nav.Link>
+                        <Nav.Link as={Link} to="about">About</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        {
+                            user ?
+                                <button className='btn btn-link text-white text-decoration-none' onClick={handleSignOut}>sign out</button>
+                                :
+                                <Nav.Link as={Link} to="login">
+                                    Login
+                                </Nav.Link>}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
